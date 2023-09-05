aa011686b6da1b1199607725e3c81461ebdfd972
SOOHYEON <soohyeon317@gmail.com>
2023-09-06 02:21:35 +0900

    fix: 커밋 누락건

--- security-jwt/src/main/kotlin/com/example/securityjwt/application/account/AccountAuthenticationCommand.kt
+++ security-jwt/src/main/kotlin/com/example/securityjwt/application/account/AccountAuthenticationCommand.kt
@@ -5,5 +5,5 @@ class AccountAuthenticationCommand {
     data class SignUp(val email: String, val password: String)
     data class SignIn(val email: String, val password: String)
     data class SignInRefresh(val accessToken: String, val refreshToken: String)
-    data class SignOut(val accessToken: String?)
+    data class SignOut(val accessToken: String)
 }
