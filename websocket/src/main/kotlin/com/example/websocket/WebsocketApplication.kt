7477ff351ef2be97f55f8a11755967416f6648b4
SOOHYEON <soohyeon317@gmail.com>
2023-09-14 14:48:46 +0900

    fix: 커밋 누락건

---
+++ websocket/src/main/kotlin/com/example/websocket/WebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketApplication>(*args)
+}
